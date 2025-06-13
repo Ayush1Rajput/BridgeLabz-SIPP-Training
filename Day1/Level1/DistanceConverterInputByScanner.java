e11de41bf81360a29f4c24ccfcde8eea8558760f
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-13 08:29:51 +0530

    Writing code for converting distance by user input

---
+++ Day1/Level1/DistanceConverterInputByScanner.java
@@ -0,0 +1,26 @@
+package day1.level1;
+
+import java.util.Scanner;
+
+class DistanceConverterInputByScanner {
+	public static void main(String[] args) {
+		// create a variable to store distance in km
+		double kilometers;
+
+		// Create scanner object to take input
+		Scanner input = new Scanner(System.in);
+
+		// Take input from user and assign to kilometers variable
+		kilometers = input.nextDouble();
+
+		// close
+		input.close();
+		  
+
+		double miles = kilometers*1.6;
+		 
+				//Display the distance in  miles
+		System.out.println("The total miles is "+ miles +
+			" mile for the given " +kilometers + " km");
+	}
+}
