0904cd1d95823eed7dc43586b7adffef7383e6b5
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-17 10:58:47 +0530

    [Ayush] Fix. fix the code of unit conversion

--- Methods/Level2/UnitConverter.java
+++ Methods/Level2/UnitConverter.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 public class UnitConverter {
+
+    public static void main(String[] args) {
+
+        // Creating the scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Getting input for kilometers to miles
+        System.out.print("Enter distance in kilometers: ");
+        double km = sc.nextDouble();
+        System.out.println("In miles: " + UnitConverter.convertKmToMiles(km));
+
+        // Getting input for miles to kilometers
+        System.out.print("Enter distance in miles: ");
+        double miles = sc.nextDouble();
+        System.out.println("In kilometers: " + UnitConverter.convertMilesToKm(miles));
+
+        // Getting input for meters to feet
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+        System.out.println("In feet: " + UnitConverter.convertMetersToFeet(meters));
+
+        // Getting input for feet to meters
+        System.out.print("Enter distance in feet: ");
+        double feet = sc.nextDouble();
+        System.out.println("In meters: " + UnitConverter.convertFeetToMeters(feet));
+
+        sc.close(); // Close the scanner
+    }
 
     // Method to convert kilometers to miles
     public static double convertKmToMiles(double km) {
