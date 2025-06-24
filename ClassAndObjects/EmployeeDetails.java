8d3eb463799fbc75ac588cdde84d1d362b868880
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-24 08:33:49 +0530

    [Ayush] Fix. write the comments on code

--- ClassAndObjects/EmployeeDetails.java
+++ ClassAndObjects/EmployeeDetails.java
@@ -1,15 +1,20 @@
 public class EmployeeDetails{
+
+    // Class defination
     static class Employee{
+        // Class attributes
         int id;
         String name;
         int salary;
 
+        // Constructer for Employee
         public Employee(String name, int id, int salary){
             this.name = name ;
             this.id = id;
             this.salary = salary;
         }
 
+        // Method to display the details of Employee
         public void display(){
             System.out.println("Employee name : "+this.name);
             System.out.println("Employee id : "+this.id);
@@ -17,9 +22,13 @@ public class EmployeeDetails{
 
         }
     }
+    // Main class
     public static void main(String[] args) {
+
+        // Creating the object of Employee
         Employee employee = new Employee("Ayush",21 , 50000);
 
+        // Calling the method for display the details
         employee.display();
     }
 }
