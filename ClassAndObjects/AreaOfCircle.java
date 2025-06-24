f76b6e34ae4da59b625c3afdb9e70356a630eb86
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-24 08:35:13 +0530

    [Ayush] Fix. write the comments on code

--- ClassAndObjects/AreaOfCircle.java
+++ ClassAndObjects/AreaOfCircle.java
@@ -1,18 +1,23 @@
 public class AreaOfCircle {
+
+    // Class defination
     static class Circle{
+        // Class attributes
         double radius;
 
-
+        // Constructer for Circle 
         public Circle(double radius){
             this.radius = radius;
         }
 
+        // Method to display the Area of circle
         public void displayArea(){
             double area = Math.PI * radius * radius; 
             System.out.println("The Area of radius "+this.radius+" is "+area);
         }
     }
     public static void main(String[] args) {
+        // Creating the object of Circle
         Circle circle = new Circle(25);
         circle.displayArea();
     }
