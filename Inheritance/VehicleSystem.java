417f3d9b16d42a3dcd275d13511da649cd6c4fc6
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-01 11:56:25 +0530

    [Ayush] Add. code for Vehicle class using inheritence and override method

---
+++ Inheritance/VehicleSystem.java
@@ -0,0 +1,82 @@
+// Superclass
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("---- Vehicle Info ----");
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+// Subclass: Car
+class Car extends Vehicle {
+    int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Subclass: Truck
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " kg");
+    }
+}
+
+// Subclass: Motorcycle
+class Motorcycle extends Vehicle {
+    boolean hasSidecar;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+
+// Main class
+public class VehicleSystem {
+    public static void main(String[] args) {
+        // Polymorphic array of Vehicles
+        Vehicle[] vehicles = new Vehicle[3];
+
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10000);
+        vehicles[2] = new Motorcycle(150, "Petrol", false);
+
+        // Dynamic method calls
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
