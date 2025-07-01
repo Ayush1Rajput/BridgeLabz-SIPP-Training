107db123609735222ea9a3f73ab5b0e24d3e4ae3
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-01 12:27:54 +0530

    [Ayush] Add. code for SmartHome class with single inheritence

---
+++ Inheritance/SmartHomeSystem.java
@@ -0,0 +1,40 @@
+// Superclass: Device
+class Device {
+    String deviceId;
+    String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("---- Device Status ----");
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+// Subclass: Thermostat
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    public Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+// Main class 
+public class SmartHomeSystem {
+    public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat("THERMO_01", "ON", 24.5);
+        thermostat.displayStatus();
+    }
+}
